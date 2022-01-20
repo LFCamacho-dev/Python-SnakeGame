afb2529f51188e872c0a1113b73289328c41890e
Luis Fernando Camacho <luisfernandoca@hotmail.com>
2022-01-20 02:05:09 -0600

    initial 3 squares moving at the same time, with a delay

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
+import time
 import turtle
 from turtle import Screen, Turtle
-
 
 screen = Screen()
 screen.setup(600, 600)
 screen.bgcolor("black")
 screen.title("Python Game")
+screen.tracer(0)
 
-turtle.speed(0)
-turtle.penup()
 
-number_of_squares = 3
 starting_positions = (0, 0), (-20, 0), (-40, 0)
 
 segments = []
@@ -23,11 +21,21 @@ for position in starting_positions:
     new_segment.goto(position)
     segments.append(new_segment)
 
+
 game_is_on = True
 
 while game_is_on:
-    for seg in segments:
-        seg.forward(20)
+    screen.update()
+    time.sleep(0.1)
+    for seg_num in range(len(segments) - 1, 0, -1):
+        new_x = segments[seg_num - 1].xcor()
+        new_y = segments[seg_num - 1].ycor()
+        segments[seg_num].goto(new_x, new_y)
+
+    segments[0].forward(20)
+
+
+
 
 
 
