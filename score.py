fbdb5dcaba39ff03432cfa352ec499656b29c745
Luis Fernando Camacho <luisfernandoca@hotmail.com>
2022-01-21 01:19:39 -0600

    added the ability to save high score to a txt file and read from it when starting the game/

--- score.py
+++ score.py
@@ -10,16 +10,34 @@ class Score(Turtle):
         self.penup()
         self.speed(0)
         self.score = 0
+        # self.high_score = 0
+        with open("hi-score.txt") as file:
+            self.high_score = int(file.read())
         self.update_scoreboard()
 
+        # File Management
+        self.hi_score_file = "hi-score.txt"
+        # self.content = self.hi_score_file.read()
+
     def update_scoreboard(self):
-        self.write(f"Score = {self.score}", False, 'center', ('Courier', 16, 'bold'))
+        self.clear()
+        self.write(f"Score = {self.score} | High Score = {self.high_score}", False, 'center', ('Courier', 16, 'bold'))
 
     def plus_one(self):
         self.score += 1
-        self.clear()
         self.update_scoreboard()
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("GAME OVER", False, "center", ('Courier', 22, 'bold'))
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            self.write_file()
+        self.score = 0
+        self.update_scoreboard()
+
+    def write_file(self):
+        with open(self.hi_score_file, "w") as file:
+            file.write(str(self.high_score))
+
+    # def game_over(self):
+    #     self.goto(0, 0)
+    #     self.write("GAME OVER", False, "center", ('Courier', 22, 'bold'))
